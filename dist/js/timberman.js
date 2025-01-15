f0afb19ee291db999e2486f977d0411312403963
yoshikobaru <hardpush123@mail.ru>
2025-01-15 04:54:23 +0300

    balansTimber

--- dist/js/timberman.js
+++ dist/js/timberman.js
@@ -166,7 +166,7 @@ function gameOver() {
 		body: JSON.stringify({
 			telegramId: window.Telegram.WebApp.initDataUnsafe.user.id,
 			amount: parseInt(score), // Убеждаемся, что score - число
-			type: 'task'
+			fromGame: true
 		})
 	})
 	.then(response => response.json())
